21192374eaa5b9a15a29f5cf0d93b8f88cb56e9f
ashghauri <143691117+ashghauri@users.noreply.github.com>
2024-08-29 11:41:36 +0500

    Adding a new file

---
+++ index.js
@@ -0,0 +1,32 @@
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment variables from .env
+
+import { GitHubClient } from './githubClient.js';
+import fs from 'fs/promises';
+
+(async () => {
+    const client = new GitHubClient();
+
+    try {
+        // // Example: Get branch info
+        // const branchInfo = await client.getBranch('main');
+        // console.log('Branch Info:', branchInfo);
+
+        // // Example: Get folder contents
+        // const folderContents = await client.getFolder('/src');
+        // console.log('Folder Contents:', folderContents);
+
+        // Example: Put a file
+        const fileContent = await fs.readFile('./index.js', 'utf8');
+        const putFileResponse = await client.putFile('index.js', fileContent, 'Adding a new file');
+        console.log('Put File Response:', putFileResponse);
+
+        // // Example: Delete a file
+        // const deleteFileResponse = await client.deleteFile('src/rec/githubClient.js');
+        // console.log('Delete File Response:', deleteFileResponse);
+
+    } catch (error) {
+        console.error('Error:', error);
+    }
+})();
